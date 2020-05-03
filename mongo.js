549fe5b42d8991aedcd80b4ad4f5af5253c8b659
Sami Särkilä <sami.sarkila@student.oulu.fi>
2020-05-03 14:14:24 +0300

    uibuild

--- mongo.js
+++ mongo.js
@@ -5,12 +5,15 @@ if (process.argv.length<3) {
   console.log('give password as argument')
   process.exit(1)
 }
-
+/*
 const password = process.argv[2]
 
 const url =
   `mongodb+srv://alikerroin:${password}@cluster0-gy21y.mongodb.net/puhelinluettelo?retryWrites=true&w=majority`
+*/
+const url = process.env.MONGODB_URI
 
+mongoose.set('useCreateIndex', true);
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
 
 const personSchema = new mongoose.Schema({
